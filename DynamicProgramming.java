4b3ea20058027e7091af1b7af1d958e6fac45df9
Melissa Thai <thaimp@rose-hulman.edu>
2015-08-27 17:18:41 -0400

    Added Best Time to Buy and Sell Stock problem

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -27,3 +27,29 @@ int climbStairs() {
 
 	return array[n];
 }
+
+// ----------------------------------------------------------------
+
+// Best Time to Buy and Sell Stock
+// Say you have an array for which the ith element is the price of a
+// given stock on day i. If you were only permitted to complete at
+// most one transaction (i.e. buy one and sell one share of the
+// stock), design an algorithm to find the maximum profit.
+// Source: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// Solution:
+int maxProfit(int[] prices) {
+	if (prices.length == 0 || prices.length == 1) {
+		return 0;
+	}
+
+	int maxProfit = 0;
+	int minBuyingPrice = prices[0];
+
+	for(int i = 1; i < prices.length; i++) {
+		minBuyingPrice = Math.min(minBuyingPrice, prices[i]);
+		maxProfit = Math.max(maxProfit, prices[i] - minBuyingPrice);
+	}
+
+	return maxProfit;
+}
