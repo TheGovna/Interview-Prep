2b57fab70a36400d6fed70f17ffabe770c7d37fb
Melissa Thai <thaimp@rose-hulman.edu>
2015-11-13 10:24:49 -0800

    removed some unnecessary code

--- LinkedLists.java
+++ LinkedLists.java
@@ -1,6 +1,5 @@
 /**
 Return Kth to Last
-Loop Detection
 Linked List Cycle
 Reverse Linked List
 Swap Nodes in Pairs
@@ -50,95 +49,9 @@ Node getKthToLastNode(Node head, int k) {
 
 // ----------------------------------------------------------------
 
-// 2.8: Loop Detection
-// Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
-
-// DEFN: Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list 
-
-// EX:
-// Input: A->B->C->D->E->C
-// Output: C
-
-// Attempt 1:
-Node findBeginning(Node head) {
-	// Create an empty array list of Nodes
-	ArrayList<Node> nodes = new ArrayList<Node>();
-	Node currentNode = head;
-
-	// For every Node, if the array list contains the Node, return it; otherwise, add it to the array list and check next node
-	while (currentNode.next != null) {
-		if (nodes.contains(currentNode)) {
-			return currentNode;
-		} else {
-			nodes.add(currentNode);
-		}
-	}
-
-	return null;
-}
-
-// Attempt 2:
-Node findBeginning(Node head) {
-	Node slow = head;
-	Node fast = head.next;
-
-	while (fast.next != null) {
-		if (slow == fast) {
-			return slow;
-		}
-
-		fast = fast.next;
-
-		if (slow == fast) {
-			return slow;
-		}
-
-		fast = fast.next;
-		slow = slow.next;
-	}
-
-	return null;
-}
-
-// ~~~ UNDERSTAND SOLUTION, IT'S WEIRD ~~~
-
-// ----------------------------------------------------------------
-
 // Linked List Cycle
 // Given a linked list, determine if it has a cycle in it.
 // Source: https://leetcode.com/problems/linked-list-cycle/
-
-// Attempt 1:
-// This works but there's a shorter way
-boolean hasCycle(Node head) {
-	if (head == null) {
-		return false;
-	}
-
-	Node fast = head;
-	Node slow = head;
-
-	if (head.next == null) {
-		return false;
-	}
-
-	if (head.next.next == null) {
-		return false;
-	}
-
-	fast = head.next.next;
-
-	while (fast.next != null && fast.next.next != null) {
-		fast = fast.next.next;
-		slow = slow.next;
-
-		if (fast == slow) {
-			return true;
-		}
-	}
-
-	return false;
-}
 
 // Solution:
 boolean hasCycle(Node head) {
